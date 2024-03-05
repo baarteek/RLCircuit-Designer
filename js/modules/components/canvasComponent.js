0f28ca21bfca9dc010ab36439120b3ec4349057b
baarteek <bartekkrukowski822@gmail.com>
2024-03-05 21:59:25 +0100

    simple scaling added

--- js/modules/components/canvasComponent.js
+++ js/modules/components/canvasComponent.js
@@ -3,6 +3,9 @@ const toolbar = document.getElementById("toolbar");
 const footer = document.getElementById("footer");
 const sidebar = document.getElementById("sidebar");
 const mousePositionText = document.getElementById("mousePositionText")
+const scaleText = document.getElementById("scaleText");
+const ctx = canvas.getContext("2d");
+let scale = 1;
 
 export const resizeCanvas = () => {
     const mainHeight = window.innerHeight - toolbar.offsetHeight - footer.offsetHeight;
@@ -15,6 +18,7 @@ export const registerCanvasEvents = () => {
     window.addEventListener("resize", resizeCanvas);
     canvas.addEventListener('mousemove', (event) => printMousePosition(event));
     canvas.addEventListener('mouseleave', () =>  mousePositionText.innerHTML = "Outside drawing area");
+    canvas.addEventListener('wheel', zoom);
 };
 
 const getMousePosition = (event) => {
@@ -28,8 +32,37 @@ const getMousePosition = (event) => {
 const printMousePosition = (event) => {
     const position = getMousePosition(event);
     if(position) {
-        mousePositionText.innerHTML = Math.floor(position.x) + ", " + Math.floor(position.y);
+        mousePositionText.innerHTML = Math.round(position.x) + ", " + Math.round(position.y);
     } else {
         mousePositionText.innerHTML = " ";
     }
+};
+
+const printScaleText = () => {
+    let rounded = parseFloat(scale.toFixed(1));
+    scaleText.innerHTML = "Scale: x" + rounded;
+};
+
+const renderScene = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    ctx.scale(scale, scale);
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.restore();
+};
+
+const zoom = (event) => {
+    event.preventDefault();
+    const scaleStep = 0.1;
+    const minScale = 0.1;
+    const maxScale = 5;
+    const direction = event.deltaY < 0 ? 1 : -1;
+    scale += direction * scaleStep;
+    scale = Math.min(Math.max(minScale, scale), maxScale);
+    renderScene();
+    printScaleText();
 };
