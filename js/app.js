5ff8a251b1917a4533adf639b55ef333299c472f
baarteek <bartekkrukowski822@gmail.com>
2024-03-05 19:37:03 +0100

    added functions responsible for events in sidebar

--- js/app.js
+++ js/app.js
@@ -1,26 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const canvas = document.getElementById("drawingCanvas");
-    const sidebar = document.getElementById("sidebar");
-    const openSidebar = document.getElementById("openSidebar");
-    const hideSlidebar = document.getElementById("hideSidebar");
+import { registerSidebarEvents } from "./modules/components/sidebar.js";
 
-    
-    function resizeCanvas() {
-        const mainHeight = window.innerHeight - toolbar.offsetHeight - footer.offsetHeight;
-        canvas.style.height = `${mainHeight}px`;
-        canvas.height = mainHeight;
-        canvas.width = sidebar.style.width === "25%" ? window.innerWidth - 250 : window.innerWidth;
-    }
-
-    window.addEventListener("resize", resizeCanvas);
-    resizeCanvas(); 
-
-    const toggleSidebar = () => {
-        const newWidth = sidebar.style.width === "25%" ? "0" : "25%";
-        sidebar.style.width = newWidth;
-        resizeCanvas();
-    };
-    
-    openSidebar.addEventListener("click", toggleSidebar);
-    hideSlidebar.addEventListener("click", toggleSidebar);
+document.addEventListener('DOMContentLoaded', (event) => {
+    registerSidebarEvents();
 });
