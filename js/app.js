64705168fc0b54370a2b172eb825acbdd9450565
baarteek <bartekkrukowski822@gmail.com>
2024-03-06 20:56:31 +0100

    added display of selected component

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import { registerCanvasEvents } from "./modules/components/canvasComponent.js";
 import { registerSidebarEvents } from "./modules/components/sidebarComponent.js";
+import { registerToolbarEvents } from "./modules/components/toolbarComponent.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     registerSidebarEvents();
     registerCanvasEvents();
+    registerToolbarEvents();
 });
---
+++ js/modules/components/toolbarComponent.js
@@ -0,0 +1,27 @@
+const components = {
+    cursor: document.getElementById("cursor"),
+    voltageSource: document.getElementById("voltageSource"),
+    switchComponent: document.getElementById("switchComponent"),
+    resistor: document.getElementById("resistor"),
+    capacitor: document.getElementById("capacitor"),
+    inducotr: document.getElementById("inductor")
+};
+
+const selectedComponent = document.getElementById("selectedComponentImg");
+
+export const registerToolbarEvents = () => {   
+    components.cursor.addEventListener("click", () => changeComponentImage("cursor"));
+    components.voltageSource.addEventListener("click", () => changeComponentImage('voltageSource'));
+    components.switchComponent.addEventListener("click", () => changeComponentImage("switch"));
+    components.resistor.addEventListener("click", () => changeComponentImage("resistor"));
+    components.capacitor.addEventListener("click", () => changeComponentImage("capacitor"));
+    components.inducotr.addEventListener("click", () => changeComponentImage("inductor"));
+};
+
+const changeComponentImage = (componentName) => {
+    if(selectedComponent) {
+        selectedComponent.src = "img/"+componentName+".png";
+    } else {
+        selectedComponent.src = "img/null.png";
+    }
+}
